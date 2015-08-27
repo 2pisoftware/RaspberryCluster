2911bc03856c07770a8e016d85879387dde6f3a9
Hamish <hamishb55@gmail.com>
2015-08-27 04:01:44 +0000

    database update

--- index.php
+++ index.php
@@ -7,30 +7,31 @@ ini_set('display_errors', 1);
 <body>
 <h1 style="text-align: center;"><?php echo $_SERVER['SERVER_ADDR']; ?></h1>
 <h2 style="text-align: center;">Requested from: <?php echo $_SERVER['REMOTE_ADDR']; ?></h2>
+
+<form action="/create.php">
+<input type="submit" value="Create New Record"><br></br>
+</form>
 <?php
-$servername = "192.168.0.79";
+$servername = "192.168.0.26";
 $username = "hamish";
 $password = "hamish";
 $dbname = "workexp";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT name, age FROM WorkExp";
+$sql = "SELECT id, name, age FROM WorkExp";
 $result = $conn->query($sql);
-
 if ($result->num_rows > 0) {
-    // output data of each row
+    echo'  <table> <tr> <th>ID</th><th>Name</th><th>Age</th><th>Actions</th> </tr>';
     while($row = $result->fetch_assoc()) {
-        echo "Name: " . $row["name"]. " - Age: " . $row["age"]. "<br>";
+	echo'<tr>  <td>' . $row['id'] . '</td><td>' . $row['name'] . '</td><td>' . $row['age'] . '</td><td>' .  "<a href='/update.php?id=" . $row['id'] . "'>update</a> <a href='/delete.php?id=" . $row['id'] . "'>delete</a>". '</td> </tr>';
     }
-} else {
-    echo "0 results";
 }
+echo '</table>';
+
 $conn->close();
 ?>
 
